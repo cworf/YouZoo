b046702e8caa0d389dba1056cb0d4d67e194d755
cworf <colin.worf@gmail.com>
2018-01-19 09:34:10 -0800

    create zoo model constructor

---
+++ src/app/zoo-model.ts
@@ -0,0 +1,13 @@
+export class Zoo {
+	constructor(
+		species: string,
+		name: string,
+		age: number,
+		diet: string,
+		location: string,
+		caretakers: number,
+		sex: string, 
+		likes: string,
+		dislikes: string
+	){}
+}
