6cf9a09d130f6936e2cb59e944cc856d9084689c
cworf <colin.worf@gmail.com>
2018-01-19 10:12:36 -0800

    create base data to work with

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Zoo } from './zoo-model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+	animals: Zoo[] = [
+		{species : "Arctic Fox", name : "Moon", age : 2, diet : "Carnivore", location : "Northern Trail", caretakers : 5, sex : "Female", likes : "Cool shade", dislikes : "Loud noise"},
+		{species : "Ocelot", name : "Prince", age : 4, diet : "Carnivore", location : "Tropical Rain Forest Building", caretakers : 6, sex : "Male", likes : "Laying in the sunshine", dislikes : "Toys that are not rope-based"},
+		{species : "Northwest Black Tailed Deer", name : "Tinkerbell", age : 8, diet : "Herbivore", location : "Northern Trail", caretakers : 2, sex : "Female", likes : "Delicate roots and leaves", dislikes : "Loud Noises"}
+	];
 }
