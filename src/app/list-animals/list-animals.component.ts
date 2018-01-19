094ec1a8160c12c742f948cfc3ec0ce4f3822162
cworf <colin.worf@gmail.com>
2018-01-19 10:46:48 -0800

    connect default data to the list animals component

--- src/app/list-animals/list-animals.component.ts
+++ src/app/list-animals/list-animals.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import { Zoo } from '../zoo-model';
 @Component({
   selector: 'app-list-animals',
   templateUrl: './list-animals.component.html',
   styleUrls: ['./list-animals.component.scss']
 })
 export class ListAnimalsComponent implements OnInit {
-
+ @Input() tempTaskList: Zoo[];
   constructor() { }
 
   ngOnInit() {
