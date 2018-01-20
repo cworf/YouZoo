20b6a8ff020841211dab9981a5ad5c4e91b2ef96
cworf <colin.worf@gmail.com>
2018-01-19 16:58:19 -0800

    add pipe function

---
+++ src/app/age.pipe.ts
@@ -0,0 +1,26 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Zoo } from './zoo-model';
+
+@Pipe({
+  name: 'age'
+})
+export class AgePipe implements PipeTransform {
+
+  transform(animalsIn: Zoo[], age): any {
+	  const youngAnimals: Zoo[] = [];
+	  const oldAnimals: Zoo[] = [];
+
+	  animalsIn.forEach(function(animal){
+		 if (animal.age < 2) {
+		     youngAnimals.push(animal);
+		 } else {
+		     oldAnimals.push(animal);
+		 }
+	  });
+	  if (age === "old") {
+	      return oldAnimals;
+	  } else {
+		  return youngAnimals;
+	  }
+  }
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { ListAnimalsComponent } from './list-animals/list-animals.component';
 import { AddAnimalComponent } from './add-animal/add-animal.component';
 import { EditAnimalComponent } from './edit-animal/edit-animal.component';
+import { AgePipe } from './age.pipe';
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { EditAnimalComponent } from './edit-animal/edit-animal.component';
     AppComponent,
     ListAnimalsComponent,
     AddAnimalComponent,
-    EditAnimalComponent
+    EditAnimalComponent,
+    AgePipe
   ],
   imports: [
     BrowserModule,
